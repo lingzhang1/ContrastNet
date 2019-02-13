e518c6ce325406e43933c61e941796f3126ab98f
Ling Zhang <dearlingzhang@gmail.com>
2019-02-13 16:04:26 -0500

    fix bug

--- train.py
+++ train.py
@@ -188,12 +188,7 @@ def train_one_epoch(sess, ops, train_writer):
         # log_string('----' + str(fn) + '-----')
         a1, a2, a_label = provider.loadDataFile_cut(TRAIN_FILES[train_file_idxs[fn]])
         if(a1.shape[0] < NUM_POINT):
-            print(a1.shape[0])
-            print(a1.shape[1])
             a1 = np.concatenate((a1, a1[0 : (NUM_POINT - a1.shape[0]), :]), axis=0)
-            print("=================")
-            print(a1.shape[0])
-            print(a1.shape[1])
         if(a2.shape[0] < NUM_POINT):
             a2 = np.concatenate((a2, a2[0 : (NUM_POINT - a2.shape[0]), :]), axis=0)
 
