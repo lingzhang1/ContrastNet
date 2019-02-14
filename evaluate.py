6f31ff08436744c01c846c93be199568007a6cad
Ling Zhang <dearlingzhang@gmail.com>
2019-02-14 11:47:43 -0500

    test

--- evaluate.py
+++ evaluate.py
@@ -142,15 +142,14 @@ def eval_one_epoch(sess, ops, num_votes=1, topk=1):
 
             loss_val, pred_val = sess.run([ops['loss'], ops['pred']],
                                       feed_dict=feed_dict)
-                                      
-            array = ops['feature'].eval(sess)
 
             batch_pred_sum += pred_val
             batch_pred_val = np.argmax(pred_val, 1)
             for el_idx in range(cur_batch_size):
                 batch_pred_classes[el_idx, batch_pred_val[el_idx]] += 1
             batch_loss_sum += (loss_val * cur_batch_size / float(num_votes))
-
+            
+        array = ops['feature'].eval(session = sess)
         for line in array:
             np.savetxt(f, line, fmt='%f')
         # pred_val_topk = np.argsort(batch_pred_sum, axis=-1)[:,-1*np.array(range(topk))-1]
