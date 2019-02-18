c98d17c4f583ccc03805832517531cafe4fc689b
Ling Zhang <dearlingzhang@gmail.com>
2019-02-17 21:45:37 -0500

    test

--- visualize.py
+++ visualize.py
@@ -9,7 +9,7 @@ read_feature = open("feature.txt", "r")
 for line in read_feature:
   line_split = line.split(" ")
   line_split = [float(i) for i in line_split]
-  X.append(float(line_split))
+  X.append(line_split)
 print(X.shape)
 X = X[:1000]
 
@@ -18,7 +18,7 @@ read_label = open("label.txt", "r")
 for line in read_label:
   line_split = line.split(" ")
   line_split = [float(i) for i in line_split]
-  y.append(float(line_split))
+  y.append(line_split)
 y = y[:1000]
 
 from sklearn.manifold import TSNE
