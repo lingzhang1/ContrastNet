2fdd00d58d0f5f5690b37c56a09e95b927f2aaa8
Ling Zhang <dearlingzhang@gmail.com>
2019-02-17 22:08:19 -0500

    test

--- visualize.py
+++ visualize.py
@@ -2,26 +2,34 @@
 # digits = datasets.load_digits()
 # # Take the first 500 data points: it's hard to see 1500 points
 # X = digits.data[:500]
-# y = digits.target[:500]
+import numpy as np
+from numpy import array
 
-X = []
+num = 3000
+NUM_POINT = 256
+X = np.empty([len(num), NUM_POINT], dtype=float)
 read_feature = open("feature.txt", "r")
+count = 0
 for line in read_feature:
   line_split = line.split(" ")
   line_split = [float(i) for i in line_split]
-  X.append(line_split)
+  X[count] = array(line_split)
+  count = count + 1
+
 print(len(X))
 X = X[:1000]
 
-y = []
+y = np.empty([len(num)], dtype=int)
 read_label = open("label.txt", "r")
+count = 0
 for line in read_label:
   line_split = line.split(" ")
   line_split = [float(i) for i in line_split]
+  y[count] = array(line_split)
   y.append(line_split)
 y = y[:1000]
 
-print(X[1, :])
+print(X[1,:])
 print(y[1])
 
 
