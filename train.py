640b7caf9c0c223d02a535890d61c22a721d8961
Ling Zhang <dearlingzhang@gmail.com>
2019-02-19 17:26:36 -0500

    restore model for training

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ parser.add_argument('--momentum', type=float, default=0.9, help='Initial learnin
 parser.add_argument('--optimizer', default='adam', help='adam or momentum [default: adam]')
 parser.add_argument('--decay_step', type=int, default=200000, help='Decay step for lr decay [default: 200000]')
 parser.add_argument('--decay_rate', type=float, default=0.7, help='Decay rate for lr decay [default: 0.8]')
-parser.add_argument('--model_path', default='log/', help='model checkpoint file path [default: log/model.ckpt]')
+parser.add_argument('--model_path', default='log/model.ckpt', help='model checkpoint file path [default: log/model.ckpt]')
 
 FLAGS = parser.parse_args()
 
@@ -135,8 +135,8 @@ def train():
         config.log_device_placement = False
         sess = tf.Session(config=config)
 
-        saver.restore(sess, tf.train.latest_checkpoint(MODEL_PATH))
-        graph = tf.get_default_graph()
+        # saver.restore(sess, tf.train.latest_checkpoint(MODEL_PATH))
+        saver.restore(sess, MODEL_PATH)
         log_string("Model restored.")
 
         # Add summary writers
