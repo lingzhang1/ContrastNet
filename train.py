41cda35bd60c636ab90cb834a8af9f9d4e88699a
Ling Zhang <dearlingzhang@gmail.com>
2019-02-20 22:57:54 -0500

    fix

--- train.py
+++ train.py
@@ -234,7 +234,7 @@ def train_one_epoch(sess, ops, train_writer):
                 current_data_1[28*count+pair_num,:,:] = total_current[index]
                 current_data_2[28*count+pair_num, :,:] = total_current[index2]
                 pair_num = pair_num + 1
-                if index < 4 && index2 >= 4:
+                if (index < 4) && (index2 >= 4):
                     current_label[28*count+pair_num,:] = 0
                 else if (index == 0 && index2 == 3) || (index == 4 && index2 == 7) || (index == 1 && index2 == 2) || (index == 5 && index2 == 6):
                     current_label[28*count+pair_num,:] = 1
