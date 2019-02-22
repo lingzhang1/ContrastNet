76ae471dafd7bc5dbdbfa0048a188038789e7339
Ling Zhang <dearlingzhang@gmail.com>
2019-02-22 16:01:43 -0500

    fix

--- evaluate.py
+++ evaluate.py
@@ -38,15 +38,15 @@ LOG_FOUT.write(str(FLAGS)+'\n')
 
 NUM_CLASSES = 5
 SHAPE_NAMES = [line.rstrip() for line in \
-    open(os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048_cut/shape_names.txt'))]
+    open(os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048_cut8/shape_names.txt'))]
 
 HOSTNAME = socket.gethostname()
 
 # ModelNet40 official train/test split
 TRAIN_FILES = provider.getDataFiles( \
-    os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048_cut/train_files.txt'))
+    os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048_cut8/train_files.txt'))
 TEST_FILES = provider.getDataFiles(\
-    os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048_cut/test_files.txt'))
+    os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048_cut8/test_files.txt'))
 
 def log_string(out_str):
     LOG_FOUT.write(out_str+'\n')
