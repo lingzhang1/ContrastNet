e3c37cfa00d137134b40badf50c950bca25d8db1
Ling Zhang <dearlingzhang@gmail.com>
2019-02-23 17:08:06 -0500

    fix

--- train.py
+++ train.py
@@ -243,10 +243,6 @@ def train_one_epoch(sess, ops, train_writer):
                 pair_num = pair_num + 1
         count = count + 1
 
-    # combine_data = np.concatenate((current_data_1, current_data_2), axis=2)
-    # combine_data, current_label, _ = provider.shuffle_data(combine_data, np.squeeze(current_label))
-    # current_data_1 = combine_data[:, :, 0:3]
-    # current_data_2 = combine_data[:, :, 3:6]
     current_label = np.squeeze(current_label)
 
     file_size = current_data_1.shape[0]
@@ -255,7 +251,7 @@ def train_one_epoch(sess, ops, train_writer):
     total_correct = 0
     total_seen = 0
     loss_sum = 0
-    print(current_data_1.shape)
+
     for batch_idx in range(num_batches):
         start_idx = batch_idx * BATCH_SIZE
         end_idx = (batch_idx+1) * BATCH_SIZE
@@ -271,27 +267,27 @@ def train_one_epoch(sess, ops, train_writer):
         label = np.squeeze(label)
 
         # Augment batched point clouds by rotation and jittering
-        rotated_data_1 = provider.rotate_point_cloud(data_1[start_idx:end_idx, :, :])
+        rotated_data_1 = provider.rotate_point_cloud(data_1)
         jittered_data_1 = provider.jitter_point_cloud(rotated_data_1)
         jittered_data_1 = provider.random_scale_point_cloud(jittered_data_1)
         jittered_data_1 = provider.rotate_perturbation_point_cloud(jittered_data_1)
         jittered_data_1 = provider.shift_point_cloud(jittered_data_1)
 
-        rotated_data_2 = provider.rotate_point_cloud(data_2[start_idx:end_idx, :, :])
+        rotated_data_2 = provider.rotate_point_cloud(data_2)
         jittered_data_2 = provider.jitter_point_cloud(rotated_data_2)
         jittered_data_2 = provider.random_scale_point_cloud(jittered_data_2)
         jittered_data_2 = provider.rotate_perturbation_point_cloud(jittered_data_2)
         jittered_data_2 = provider.shift_point_cloud(jittered_data_2)
-        print(jittered_data_1.shape)
+
         feed_dict = {ops['pointclouds_pl_1']: jittered_data_1,
                      ops['pointclouds_pl_2']: jittered_data_2,
-                     ops['labels_pl']: label[start_idx:end_idx],
+                     ops['labels_pl']: label,
                      ops['is_training_pl']: is_training,}
         summary, step, _, loss_val, pred_val = sess.run([ops['merged'], ops['step'],
             ops['train_op'], ops['loss'], ops['pred']], feed_dict=feed_dict)
         train_writer.add_summary(summary, step)
         pred_val = np.argmax(pred_val, 1)
-        correct = np.sum(pred_val == label[start_idx:end_idx])
+        correct = np.sum(pred_val == label)
         total_correct += correct
         total_seen += BATCH_SIZE
         loss_sum += loss_val
