60953d48dacd4253b6f5d63ad9cd6232c34e7a53
Ling Zhang <dearlingzhang@gmail.com>
2019-02-23 12:00:08 -0500

    push

--- evaluate.py
+++ evaluate.py
@@ -38,15 +38,15 @@ LOG_FOUT.write(str(FLAGS)+'\n')
 
 NUM_CLASSES = 4
 SHAPE_NAMES = [line.rstrip() for line in \
-    open(os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048_cut8/shape_names.txt'))]
+    open(os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048_cut4/shape_names.txt'))]
 
 HOSTNAME = socket.gethostname()
 
 # ModelNet40 official train/test split
 TRAIN_FILES = provider.getDataFiles( \
-    os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048_cut8/train_files.txt'))
+    os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048_cut4/train_files.txt'))
 TEST_FILES = provider.getDataFiles(\
-    os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048_cut8/test_files.txt'))
+    os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048_cut4/test_files.txt'))
 
 def log_string(out_str):
     LOG_FOUT.write(out_str+'\n')
@@ -104,11 +104,11 @@ def eval_one_epoch(sess, ops, feature_f, num_votes=1, topk=1):
 
     for fn in range(len(TEST_FILES)):
         # log_string('----'+str(fn)+'----')
-        # cut1, cut2, cut3, cut4, label = provider.loadDataFile_cut_4(TEST_FILES[fn])
-        # data = np.concatenate((cut1, cut2, cut3, cut4), axis=0)
+        cut1, cut2, cut3, cut4, label = provider.loadDataFile_cut_4(TEST_FILES[fn])
+        data = np.concatenate((cut1, cut2, cut3, cut4), axis=0)
 
-        total, label = provider.loadDataFile_cut(TEST_FILES[fn])
-        data = np.concatenate((total[0], total[1], total[2], total[3], total[4], total[5], total[6], total[7]), axis=0)
+        # total, label = provider.loadDataFile_cut(TEST_FILES[fn])
+        # data = np.concatenate((total[0], total[1], total[2], total[3], total[4], total[5], total[6], total[7]), axis=0)
 
         idx = np.random.randint(data.shape[0], size=NUM_POINT)
         data = data[idx,:]
