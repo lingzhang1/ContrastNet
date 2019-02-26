a9b085a8a4bf8908a537b7ba34a00c034fccc74e
Ling Zhang <dearlingzhang@gmail.com>
2019-02-25 21:08:53 -0500

    fix

--- provider.py
+++ provider.py
@@ -194,7 +194,7 @@ def load_cut2_random_h5(h5_filename):
 
 def load_cut2_h5(h5_filename):
   f = h5py.File(h5_filename)
-  random_id = np.zeros(1)
+  random_id = np.zeros((1,), dtype=int)
   # random_id = np.random.randint(30, size=1)
   random_id = random_id + 1
   if random_id[0] % 2 == 0:
