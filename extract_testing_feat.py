9f79dcf635812ec88bb41fba6ea60999d09ec06f
Ling Zhang <dearlingzhang@gmail.com>
2019-03-04 07:03:14 -0500

    fix

--- extract_testing_feat.py
+++ extract_testing_feat.py
@@ -62,7 +62,7 @@ def evaluate(num_votes):
         is_training_pl = tf.placeholder(tf.bool, shape=())
         # simple model
         # pred, feature1, feature2, end_points = MODEL.get_model(pointclouds_pl, pointclouds_pl, is_training_pl)
-        pred, feature, end_points = MODEL.get_model(pointclouds_pl, is_training_pl)
+        pred, feature1, end_points = MODEL.get_model(pointclouds_pl, is_training_pl)
         loss = MODEL.get_loss(pred, labels_pl, end_points)
         # Add ops to save and restore all the variables.
         saver = tf.train.Saver()
@@ -82,7 +82,7 @@ def evaluate(num_votes):
            'is_training_pl': is_training_pl,
            'pred': pred,
            'loss': loss,
-           'feature': feature}
+           'feature': feature1}
 
     #save labels for test
     feature_f = open('feature.txt', 'w+')
