bb0bef3234da4abe5f85841645d7b70cdfe9e998
Ling Zhang <dearlingzhang@gmail.com>
2019-03-03 21:06:41 -0500

    cross

--- extract_testing_feat.py
+++ extract_testing_feat.py
@@ -62,7 +62,7 @@ def evaluate(num_votes):
         is_training_pl = tf.placeholder(tf.bool, shape=())
         # simple model
         pred, feature1, feature2, end_points = MODEL.get_model(pointclouds_pl, pointclouds_pl, is_training_pl)
-        loss = MODEL.get_constra_cross_loss(pred, feature1, feature2, labels_pl, end_points)
+        loss = MODEL.get_loss(pred, labels_pl, end_points)
         # Add ops to save and restore all the variables.
         saver = tf.train.Saver()
 
