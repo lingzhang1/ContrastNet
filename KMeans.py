d60a1b32e17571b0f06a732d81ae000531954cd2
Ling Zhang <dearlingzhang@gmail.com>
2019-03-26 21:43:07 -0400

    fix

--- KMeans.py
+++ KMeans.py
@@ -8,9 +8,9 @@ NUM_CLASS = 40
 # train_X = np.empty([TRAIN_NUM, NUM_POINT], dtype=float)
 train_X = []
 read_feature = open("train_cluster.txt", "r")
-count = 0
 
 lines = read_feature.readlines()
+print(len(lines))
 for line in lines:
   line_split = line.split(" ")
   line_split = [float(i) for i in line_split]
