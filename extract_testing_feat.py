07f79fec5030f004efa6270315ff2c5d4ff66dcc
Ling Zhang <dearlingzhang@gmail.com>
2019-03-27 14:53:35 -0400

    get average featrue

--- extract_testing_feat.py
+++ extract_testing_feat.py
@@ -148,7 +148,7 @@ def eval_one_epoch(sess, ops, feature_f, num_votes=12, topk=1):
                                   feed_dict=feed_dict)
 
         feat_mean = tf.reduce_mean(feat_out, 0)  # [1.5, 1.5]
-
+        feat_mean = tf.expand_dims(feat_mean, 1)
         print('feat_mean = ', feat_mean.shape)
         np.savetxt(feature_f, feat_mean, fmt='%f')
 
