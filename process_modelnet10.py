e5a6ffc13eb19bdf0080a3c55424641886db2f0d
Ling Zhang <dearlingzhang@gmail.com>
2019-04-08 23:45:40 -0400

    fix

--- process_modelnet10.py
+++ process_modelnet10.py
@@ -25,22 +25,22 @@ for c in class_folders:
     test_name = np.array(test_name)
     print('test_name[0] = ',test_name[0])
     np.savetxt(f_test, test_name, fmt='%s')
-    for name in test_files:
-        path = join(test_path, name)
-        print('path = ', path)
-        f = open(path, 'w+')
-        lines = f.readlines()
-        lines = lines[2:]
-        print(lines)
-        print('lines = ',len(lines))
-        data = []
-        for l in lines:
-            l = l.split(' ')
-            l = [float(i) for i in l]
-            data.append(l)
-        data = np.array(data)
-        hf = h5py.File(join(out_folder, name[0:-4]+'.h5'), 'w')
-        hf.create_dataset('data', data=data)
+    # for name in test_files:
+    #     path = join(test_path, name)
+    #     print('path = ', path)
+    #     f = open(path, 'w+')
+    #     lines = f.readlines()
+    #     lines = lines[2:]
+    #     print(lines)
+    #     print('lines = ',len(lines))
+    #     data = []
+    #     for l in lines:
+    #         l = l.split(' ')
+    #         l = [float(i) for i in l]
+    #         data.append(l)
+    #     data = np.array(data)
+    #     hf = h5py.File(join(out_folder, name[0:-4]+'.h5'), 'w')
+    #     hf.create_dataset('data', data=data)
 
     train_path = join(c, 'train')
     train_files = [f for f in listdir(train_path) if isfile(join(train_path, f))]
@@ -48,17 +48,17 @@ for c in class_folders:
     train_name = [join(out_folder, f[0:-4]+'.h5') for f in listdir(train_path) if isfile(join(train_path, f))]
     train_name = np.array(train_name)
     np.savetxt(f_train, train_name, fmt='%s')
-    for name in train_files:
-        path = join(train_path, name)
-        f = open(path, 'w+')
-        lines = f.readlines()
-        lines = lines[2:]
-        data = []
-        for l in lines:
-            l = l.split(' ')
-            l = [float(i) for i in l]
-            data.append(l)
-        data = np.array(data)
-
-        hf = h5py.File(join(out_folder, name[0:-4]+'.h5'), 'w')
-        hf.create_dataset('data', data=data)
+    # for name in train_files:
+    #     path = join(train_path, name)
+    #     f = open(path, 'w+')
+    #     lines = f.readlines()
+    #     lines = lines[2:]
+    #     data = []
+    #     for l in lines:
+    #         l = l.split(' ')
+    #         l = [float(i) for i in l]
+    #         data.append(l)
+    #     data = np.array(data)
+    #
+    #     hf = h5py.File(join(out_folder, name[0:-4]+'.h5'), 'w')
+    #     hf.create_dataset('data', data=data)
